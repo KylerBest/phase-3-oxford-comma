eff6d11c62adbd4149fc092ff0b1fa0c19dc33f2
Kyler Best <kyler.b@live.com>
2022-02-18 20:00:17 -0600

    commit tree 25a3a5dab1a9f6258cbf4c57d10d42bfb92c2de4

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
-
+def oxford_comma array
+    if array.length == 1
+        return array[0]
+    elsif array.length == 2
+        return "#{array[0]} and #{array[1]}"
+    else
+        result = ""
+        array.each.with_index do |word, i|
+            result += (i == array.length - 1 ? "and #{word}" : "#{word}, ")
+        end
+    end
+    result
 end
